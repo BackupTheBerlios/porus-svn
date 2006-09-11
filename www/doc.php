49700c8cfb5c8b4c6d16cabf3a2be37432d324f0
mpashton <mpashton@a717f349-1ee6-0310-a0f5-f21bd5acc0cd>
2006-09-11 04:42:03 +0000

    web update

--- www/doc.php
+++ www/doc.php
@@ -1,8 +1,10 @@
 <?php $title="documentation"; include 'header.php'; ?>
 
 <p>Sadly, documentation is a bit thin on the ground at the moment.  We
-do have some Doxygen you can look at:</p>
+do have some <a href="http://doxygen.org/">Doxygen</a> you can look at:</p>
 
 <p><b><a href="doxygen">Doxygen-generated docs</a></b></p>
+
+<p>The <a href="README">README</a> also has some useful notes.</p>
 
 <?php include 'footer.php'; ?>
--- www/index.php
+++ www/index.php
@@ -1,10 +1,18 @@
 <?php $title="about"; include 'header.php'; ?>
 
+<h2>What's PORUS?</h2>
+
 <p>PORUS is a portable <a href="http://www.usb.org">USB</a> stack for USB peripherals.  It handles enumeration and many standard control requests, and provides a portable API for performing USB transactions.</p>
 
 <p>See the <a href="faq.php">FAQ</a> for a quick introduction to PORUS.</p>
 
+<h3>Download</h3>
+
 <p>PORUS is distributed as source only.  (This isn't because I'm a source-code fanatic; it's because it's not practical to distribute PORUS as a binary.)  Download it from the
 <a href="http://developer.berlios.de/project/showfiles.php?group_id=7232">files page</a>.</p>
+
+<h3>Contact</h3>
+
+<p>Contact Michael Ashton at &lt; mpashton at users dot berlios dot de &gt; for information.  (Change at to @ and dot to . to decode the secret email.  This is to help fool spam harvesters.)</p>
 
 <?php include 'footer.php'; ?>
